08c78e27eb44497e41d7808082a6e7a00e180fad
Muntazir Naqvi <muhammad.muntazir.naqvi@nmbu.no>
2021-06-08 02:07:25 +0200

    initial commit - tests for cell and island classes

---
+++ tests/test_cell_class.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Test set for Cells class for INF200 June 2021.
+"""
+
+import pytest
+
+def test_add_animal():
+    pass
+
+def test_reset_cell():
+    pass
---
+++ tests/test_island_class.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Test set for Island class for INF200 June 2021.
+"""
+
+import pytest
+
+def test_make_map():
+    pass
+
