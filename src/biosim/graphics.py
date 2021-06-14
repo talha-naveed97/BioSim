f6e1ea8f6e3515b9bfbe23fcec5d4df13cf2a110
talhanaveed97 <59391648+talhanaveed97@users.noreply.github.com>
2021-06-14 18:41:06 +0200

    Update graphics.py

--- src/biosim/graphics.py
+++ src/biosim/graphics.py
@@ -20,18 +20,14 @@ _DEFAULT_MOVIE_FORMAT = 'mp4'  # alternatives: mp4, gif
 
 class Graphics:
     def __init__(self, img_dir=None, img_name=None, img_fmt=None):
-        self.plots = [{'Name': 'Geography', 'Plot': None, 'row': 1, 'Position': 1, 'title': "Island"},
-                      {'Name': 'Number_of_species', 'Plot': None, 'row': 1, 'Position': 3, 'title': "Animal Count"},
-                      {'Name': 'Herbivore_Distribution', 'Plot': None, 'row': 2, 'Position': 1,
-                       'title': "Herbivore "
-                                "Distribution"},
-                      {'Name': 'Carnivore_Distribution', 'Plot': None, 'row': 2, 'Position': 3,
-                       'title': "Carnivore "
-                                "Distribution"},
-                      {'Name': 'Fitness_Histogram', 'Plot': None, 'row': 3, 'Position': 1, 'title': "Fitness"},
-                      {'Name': 'Age_Histogram', 'Plot': None, 'row': 3, 'Position': 2, 'title': "Age"},
-                      {'Name': 'Weight_Histogram', 'Plot': None, 'row': 3, 'Position': 3, 'title': "Weight"},
-                      ]
+        self.fig = None
+        self.geography_ax = None
+        self.species_count_ax = None
+        self.herbivore_dist_ax = None
+        self.carnivore_dist_ax = None
+        self.fitness_hist_ax = None
+        self.age_hist_ax = None
+        self.weight_hist_ax = None
         self.line_herbivores = []
         self.line_carnivores = []
         self.herb_dist_axis = None
@@ -40,7 +36,6 @@ class Graphics:
         self.year_counter = None
         self.year_txt = None
         self.year_template = 'Year: {:5d}'
-        self.fig = None
         self._gs = None
         self.img_dir = img_dir
 
@@ -67,10 +62,13 @@ class Graphics:
                             img_years, map_rgb, herb_dist, carn_dist):
         self.fig = plt.figure(constrained_layout=True)
         self._gs = gridspec.GridSpec(ncols=rows, nrows=cols, figure=self.fig)
-        for plot in self.plots:
-            ax = self.fig.add_subplot(self._gs[plot['row'] - 1, plot['Position'] - 1])
-            ax.set_title(plot['title'], fontweight='bold')
-            plot['Plot'] = ax
+        self.geography_ax = self.fig.add_subplot(self._gs[0, 0])
+        self.species_count_ax = self.fig.add_subplot(self._gs[0, 2])
+        self.herbivore_dist_ax = self.fig.add_subplot(self._gs[1, 0])
+        self.carnivore_dist_ax = self.fig.add_subplot(self._gs[1, 2])
+        self.fitness_hist_ax = self.fig.add_subplot(self._gs[2, 0])
+        self.age_hist_ax = self.fig.add_subplot(self._gs[2, 1])
+        self.weight_hist_ax = self.fig.add_subplot(self._gs[2, 2])
         self.make_map(map_rgb)
         self.update_number_of_species_graph(True, 0, total_years, 0, 0, y_max)
         self.update_distribution_map(herb_dist, carn_dist, cmap)
@@ -84,27 +82,15 @@ class Graphics:
                                                verticalalignment='center',
                                                transform=self.year_counter.transAxes, fontsize=16)
         self.img_years = img_years
-        # if self.img_dir is not None and os.path.exists(self.img_dir):
-        #     for filename in os.listdir(self.img_dir):
-        #         file_path = os.path.join(self.img_dir, filename)
-        #         try:
-        #             if os.path.isfile(file_path) or os.path.islink(file_path):
-        #                 os.unlink(file_path)
-        #             elif os.path.isdir(file_path):
-        #                 shutil.rmtree(file_path)
-        #         except Exception as e:
-        #             print('Failed to delete %s. Reason: %s' % (file_path, e))
-
         if not os.path.exists(self.img_dir):
             os.mkdir(self.img_dir)
 
         self.show_plots()
 
     def make_map(self, map_rgb):
-        ax = [pt['Plot'] for pt in self.plots if pt['Name'] == 'Geography'][0]
-        ax.imshow(map_rgb)
-        ax.set_xticks(np.arange(0, len(map_rgb[0]), 5))
-        ax.set_yticks(np.arange(0, len(map_rgb), 2))
+        self.geography_ax.imshow(map_rgb)
+        self.geography_ax.set_xticks(np.arange(0, len(map_rgb[0]), 5))
+        self.geography_ax.set_yticks(np.arange(0, len(map_rgb), 2))
 
     def update_visualization(self, year, total_years, cmax_animals, hist_specs, y_max,
                              herbivore_data, carnivore_data):
@@ -124,25 +110,24 @@ class Graphics:
 
     def update_number_of_species_graph(self, is_init, current_year, total_years,
                                        herbivore_count, carnivores_count, y_max):
-        ax = [pt['Plot'] for pt in self.plots if pt['Name'] == 'Number_of_species'][0]
         max_count = max(herbivore_count, carnivores_count)
         if y_max is not None:
             self.max_species_count = y_max
         if is_init:
-            ax.set_xlim(0, total_years)
-            ax.set_ylim(0, self.max_species_count)
-            self.line_herbivores = ax.plot(np.arange(total_years),
-                                           np.full(total_years, np.nan), 'b-', label='Herbivores')[0]
-            self.line_carnivores = ax.plot(np.arange(total_years),
-                                           np.full(total_years, np.nan), 'r-', label='Carnivores')[0]
-            ax.legend()
+            self.species_count_ax.set_xlim(0, total_years)
+            self.species_count_ax.set_ylim(0, self.max_species_count)
+            self.line_herbivores = self.species_count_ax.plot(np.arange(total_years),
+                                                              np.full(total_years, np.nan), 'b-', label='Herbivores')[0]
+            self.line_carnivores = self.species_count_ax.plot(np.arange(total_years),
+                                                              np.full(total_years, np.nan), 'r-', label='Carnivores')[0]
+            self.species_count_ax.legend()
         else:
             if total_years > len(self.line_herbivores.get_xdata()):
                 x_data_h, y_data_h = self.line_herbivores.get_data()
                 x_data_c, y_data_c = self.line_carnivores.get_data()
                 x_new_h = np.arange(x_data_h[-1] + 1, total_years + 1)
                 x_new_c = np.arange(x_data_c[-1] + 1, total_years + 1)
-                ax.set_xlim(0, total_years + 1)
+                self.species_count_ax.set_xlim(0, total_years + 1)
                 if len(x_new_h) > 0:
                     y_new_h = np.full(x_new_h.shape, np.nan)
                     self.line_herbivores.set_data(np.hstack((x_data_h, x_new_h)),
@@ -153,7 +138,7 @@ class Graphics:
                                                   np.hstack((y_data_c, y_new_c)))
             if self.max_species_count < max_count:
                 self.max_species_count = int(1.2 * max_count)
-                ax.set_ylim(0, self.max_species_count)
+                self.species_count_ax.set_ylim(0, self.max_species_count)
         y_data_herbivore = self.line_herbivores.get_ydata()
         y_data_herbivore[current_year - 1] = herbivore_count
         y_data_carnivore = self.line_carnivores.get_ydata()
@@ -165,56 +150,51 @@ class Graphics:
         if self.herb_dist_axis is not None:
             self.herb_dist_axis.set_data(herbivore_map)
         else:
-            ax_herb = [pt['Plot'] for pt in self.plots if pt['Name'] == 'Herbivore_Distribution'][0]
-            self.herb_dist_axis = ax_herb.imshow(herbivore_map,
-                                                 interpolation='nearest',
-                                                 vmin=-0.25, vmax=cmax_animals['Herbivore'],
-                                                 )
-            plt.colorbar(self.herb_dist_axis, ax=ax_herb,
+            self.herb_dist_axis = self.herbivore_dist_ax.imshow(herbivore_map,
+                                                                interpolation='nearest',
+                                                                vmin=-0.25, vmax=cmax_animals['Herbivore'],
+                                                                )
+            plt.colorbar(self.herb_dist_axis, ax=self.herbivore_dist_ax,
                          orientation='vertical')
 
         if self.carn_dist_axis is not None:
             self.carn_dist_axis.set_data(carnivore_map)
         else:
-            ax_carn = [pt['Plot'] for pt in self.plots if pt['Name'] == 'Carnivore_Distribution'][0]
-            self.carn_dist_axis = ax_carn.imshow(carnivore_map,
-                                                 interpolation='nearest',
-                                                 vmin=-0.25, vmax=cmax_animals['Carnivore'],
-                                                 )
-            plt.colorbar(self.carn_dist_axis, ax=ax_carn,
+            self.carn_dist_axis = self.carnivore_dist_ax.imshow(carnivore_map,
+                                                                interpolation='nearest',
+                                                                vmin=-0.25, vmax=cmax_animals['Carnivore'],
+                                                                )
+            plt.colorbar(self.carn_dist_axis, ax=self.carnivore_dist_ax,
                          orientation='vertical')
 
     def update_fitness_histogram(self, fitness_values_herbivores,
                                  fitness_values_carnivores, fitness_hist_specs):
-        pt = [pt for pt in self.plots if pt['Name'] == 'Fitness_Histogram'][0]
-        pt['Plot'].clear()
-        pt['Plot'].set_title(pt['title'], fontweight='bold')
-        bins = int(fitness_hist_specs['max'] / fitness_hist_specs['delta'])
-        pt['Plot'].hist((fitness_values_herbivores, fitness_values_carnivores), bins,
-                        (0, fitness_hist_specs['max']), histtype='step', linewidth=2,
-                        label=('Herbivores', 'Carnivores'), color=('b', 'r'))
-        pt['Plot'].legend()
+        self.fitness_hist_ax.clear()
+        self.fitness_hist_ax.set_title("Fitness", fontweight='bold')
+        bins = get_bins(fitness_hist_specs)
+        self.fitness_hist_ax.hist((fitness_values_herbivores, fitness_values_carnivores), bins,
+                                  (0, fitness_hist_specs['max']), histtype='step', linewidth=2,
+                                  label=('Herbivores', 'Carnivores'), color=('b', 'r'))
+        self.fitness_hist_ax.legend()
 
     def update_age_histogram(self, age_values_herbivores, age_values_carnivores, age_hist_specs):
-        pt = [pt for pt in self.plots if pt['Name'] == 'Age_Histogram'][0]
-        pt['Plot'].clear()
-        pt['Plot'].set_title(pt['title'], fontweight='bold')
-        bins_age = int(age_hist_specs['max'] / age_hist_specs['delta'])
-        pt['Plot'].hist((age_values_herbivores, age_values_carnivores), bins_age,
-                        (0, age_hist_specs['max']), histtype='step', linewidth=2,
-                        label=('Herbivores', 'Carnivores'), color=('b', 'r'))
-        pt['Plot'].legend()
+        self.age_hist_ax.clear()
+        self.age_hist_ax.set_title("Age", fontweight='bold')
+        bins = get_bins(age_hist_specs)
+        self.age_hist_ax.hist((age_values_herbivores, age_values_carnivores), bins,
+                              (0, age_hist_specs['max']), histtype='step', linewidth=2,
+                              label=('Herbivores', 'Carnivores'), color=('b', 'r'))
+        self.age_hist_ax.legend()
 
     def update_weight_histogram(self, weight_values_herbivores,
                                 weight_values_carnivores, weight_hist_specs):
-        pt = [pt for pt in self.plots if pt['Name'] == 'Weight_Histogram'][0]
-        pt['Plot'].clear()
-        pt['Plot'].set_title(pt['title'], fontweight='bold')
-        bins = int(weight_hist_specs['max'] / weight_hist_specs['delta'])
-        pt['Plot'].hist((weight_values_herbivores, weight_values_carnivores), bins,
-                        (0, weight_hist_specs['max']), histtype='step', linewidth=2,
-                        label=('Herbivores', 'Carnivores'), color=('b', 'r'))
-        pt['Plot'].legend()
+        self.weight_hist_ax.clear()
+        self.weight_hist_ax.set_title("Weight", fontweight='bold')
+        bins = get_bins(weight_hist_specs)
+        self.weight_hist_ax.hist((weight_values_herbivores, weight_values_carnivores), bins,
+                                 (0, weight_hist_specs['max']), histtype='step', linewidth=2,
+                                 label=('Herbivores', 'Carnivores'), color=('b', 'r'))
+        self.weight_hist_ax.legend()
 
     def _save_graphics(self, year):
         """Saves graphics to file if file name given."""
@@ -267,3 +247,7 @@ class Graphics:
                 raise RuntimeError('ERROR: convert failed with: {}'.format(err))
         else:
             raise ValueError('Unknown movie format: ' + movie_fmt)
+
+
+def get_bins(spec):
+    return int(spec['max'] / spec['delta'])
